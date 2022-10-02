294ccf2b5f75cd5e30cda5824db18544eab05c70
V0rt3x667 <al-g0l@outlook.com>
2022-10-02 04:56:14 +0100

    Updated

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-function download() {
-    curl -sL -O -C - "$1"
-}
-
 function addPackages() {
     local packages=(
         'binutils'
@@ -26,19 +22,6 @@ function addPackages() {
         'util-linux'
     )
     apk add --no-cache "${packages[@]}"
-}
-
-function addKernels() {
-    local kver
-    local kurl="https://api.github.com/repos/M0Rf30/qemu-kernel-$MODEL/releases"
-
-    mkdir /tmp && cd /tmp
-    kver="$(download "$kurl"/latest | grep -m 1 tag_name | cut -d\" -f4)"
-
-    download "$kurl/download/$kver/qemu_kernel_$MODEL-$kver"
-    install -Dm644 "/tmp/qemu_kernel_rpi_$MODEL-$kver" -t "/opt/victorpi/kernel/qemu-kernel-$MODEL"
-
-    rm -rf /tmp/*
 }
 
 function addNetworking() {
@@ -76,39 +59,6 @@ function addVictorpi() {
     rm -rf /tmp/*
 }
 
-function addAAVFM() {
-    local arch
-    local fedurl="https://kojipkgs.fedoraproject.org//packages/edk2"
-    local fedver=38
-    local pkgrel=1
-    local pkgver=20220826gitba0e0e4c6a17
-
-    if [[ -d "$OVMFPATH" ]]; then
-        return
-    else
-        mkdir -p "$OVMFPATH"
-    fi
-
-    if [[ "$MODEL" = "rpi-2" ]]; then
-        cd "$OVMFPATH"
-        arch=arm
-        download "$fedurl/$pkgver/$pkgrel.fc$fedver/noarch/edk2-$arch-$pkgver-$pkgrel.fc$fedver.noarch.rpm"
-        rpm2cpio ./*.noarch.rpm | cpio -idmv
-        cd /usr/share/AAVMF
-        ln -sf ../edk2/arm/vars-template-pflash.raw AAVMF32_VARS.fd
-    elif [[ "$MODEL" = "rpi-3" ]]; then
-        cd "$OVMFPATH"
-        arch=aarch64
-        download "$fedurl/$pkgver/$pkgrel.fc$fedver/noarch/edk2-$arch-$pkgver-$pkgrel.fc$fedver.noarch.rpm"
-        rpm2cpio ./*.noarch.rpm | cpio -idmv
-        cd /usr/share/AAVMF
-        ln -sf ../edk2/arm/vars-template-pflash.raw AAVMF32_VARS.fd
-    fi
-}
-
-echo "===> Download & Copy RPi Kernels"
-addKernels
-
 echo "===> Networking Settings ..."
 addNetworking
 
@@ -117,8 +67,5 @@ addPackages
 
 echo "===> Installing VictorPi"
 addVictorpi
-
-echo "===> Installing AVMF ARM & AARCH64"
-addAAVFM
 
 echo "===> DONE $0 $*"
